67a00dc5205c283183eb23436c923152c6ccb443
Daniel Law <daniel@inl.in.th>
2025-08-18 00:20:57 +0700

    Add metrics for SSTable creation and implement quarantine mechanism for corrupted SSTables

--- engine/metrics.go
+++ engine/metrics.go
@@ -19,6 +19,7 @@ type EngineMetrics struct {
 	FlushTotal            *expvar.Int
 	CompactionTotal       *expvar.Int
 	CompactionErrorsTotal *expvar.Int // New
+	SSTablesCreatedTotal  *expvar.Int // New: Total number of SSTables created
 
 	FlushDataPointsFlushedTotal *expvar.Int // New: Total data points flushed
 	FlushBytesFlushedTotal      *expvar.Int // New: Total bytes flushed
@@ -114,6 +115,7 @@ func NewEngineMetrics(publishGlobally bool, prefix string) *EngineMetrics {
 		FlushTotal:            newIntFunc(prefix + "flush_total"),
 		CompactionTotal:       newIntFunc(prefix + "compaction_total"),
 		CompactionErrorsTotal: newIntFunc(prefix + "compaction_errors_total"),
+		SSTablesCreatedTotal:  newIntFunc(prefix + "sstables_created_total"),
 
 		FlushDataPointsFlushedTotal:    newIntFunc(prefix + "flush_data_points_flushed_total"),
 		FlushBytesFlushedTotal:         newIntFunc(prefix + "flush_bytes_flushed_total"),
